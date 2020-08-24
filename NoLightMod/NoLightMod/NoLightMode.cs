97ca1355e16e3da2cc2afc2c09ba438aa7aed4ef
Alexis Schwindt <alexis.schwindt@outlook.com>
2020-08-24 00:46:31 -0300

    Created no light mode
    Mod created and suposedly working right.

---
+++ NoLightMod/NoLightMod/NoLightMode.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using UnityEngine;
+using MelonLoader;
+using Harmony;
+using Managers;
+using TwitchAPI;
+using Menu;
+using Network;
+using UnityEngine.SceneManagement;
+using UnhollowerBaseLib;
+using UnhollowerRuntimeLib;
+
+
+namespace NoLightMod
+{
+    public class NoLightMode : MelonMod
+    {
+        public class ModInfo
+        {
+            public const string GUID = "com.kittyskin.hellpoint.NoLightMod";
+            public const string NAME = "No Ligh Mode";
+            public const string AUTHOR = "Kitty Skin";
+            public const string VERSION = "1.0.0";
+            public const string GAME_NAME = "Hellpoint";
+            public const string GAME_COMPANY = "Cradle Games";
+        }
+
+ 
+        public static bool InMenu => SceneManager.GetActiveScene().name.ToLower().Contains("empty");
+        private static bool m_noCharacter = true;
+        private float lightTimer = 0f;
+        private float TurnOffTimer = 5f;
+
+
+        public VoteCommand VoteManager => m_voteManager ?? (m_voteManager = TwitchManager.Instance?.GetComponentInChildren<VoteCommand>());
+        private VoteCommand m_voteManager;
+
+        // Update and randomize time
+
+        public override void OnUpdate()
+        {
+            if (LoadingView.Loading || m_noCharacter || InMenu)
+            {
+                TurnOffTimer = 5f;
+                return;
+            }
+
+            lightTimer += Time.deltaTime;
+
+            if (lightTimer > TurnOffTimer)
+            {
+                TurnOffLight();
+                lightTimer -= TurnOffTimer;
+                TurnOffTimer = 3600f;
+            }
+        }
+
+        private void TurnOffLight()
+        {
+            var voteCommand = TwitchManager.Instance.GetComponentInChildren<VoteCommand>();
+            voteCommand.GetComponentInChildren<VoteTurnOffLightOption>().Apply();
+            voteCommand.GetComponentInChildren<VoteTurnOffLightOption>().duration = +3600;
+        }
+
+        // Patches to track gameplay state
+
+        [HarmonyPatch(typeof(SplashView), nameof(SplashView.OnSaveSelected))]
+        public class SplashView_OnSaveSelected
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                m_noCharacter = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(SplashView), nameof(SplashView.NewGame))]
+        public class SplashView_NewGame
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                m_noCharacter = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(SystemView), nameof(SystemView.SaveAndQuit))]
+        public class SystemView_SaveAndQuit
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                m_noCharacter = true;
+            }
+        }
+
+        [HarmonyPatch(typeof(SystemView), nameof(SystemView.QuitWithoutSaving))]
+        public class SystemView_QuitWithoutSaving
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                m_noCharacter = true;
+            }
+        }
+
+    }
+}
---
+++ NoLightMod/NoLightMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using NoLightMod;
+
+[assembly: MelonInfo(typeof(NoLightMode), NoLightMode.ModInfo.NAME, NoLightMode.ModInfo.VERSION, NoLightMode.ModInfo.AUTHOR)]
+[assembly: MelonGame(NoLightMode.ModInfo.GAME_COMPANY, NoLightMode.ModInfo.GAME_NAME)]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoLightMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("*")]
+[assembly: AssemblyProduct("NoLightMod")]
+[assembly: AssemblyCopyright("Copyright © * 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a62b54ff-dc82-40a8-b4b3-5185297826fb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
