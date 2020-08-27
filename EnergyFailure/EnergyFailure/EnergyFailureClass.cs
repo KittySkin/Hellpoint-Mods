62893a84879a4b1ad64fe69eda4aaa3d478f8d46
Alexis Schwindt <alexis.schwindt@outlook.com>
2020-08-27 00:26:44 -0300

    Added ghost function to energy failure mod
    added ghost function to energy failure mode, added random duration to full shutdowns as well as random ammount of flickers.
    Also added configs to enable or disable the ghost feature

--- EnergyFailure/EnergyFailure/EnergyFailureClass.cs
+++ EnergyFailure/EnergyFailure/EnergyFailureClass.cs
@@ -17,7 +17,6 @@ using UnhollowerBaseLib;
 using UnhollowerRuntimeLib;
 using Debugs;
 
-
 namespace EnergyFailure
 {
 public class EnergyFailureClass : MelonMod
@@ -32,13 +31,22 @@ public class EnergyFailureClass : MelonMod
             public const string GAME_COMPANY = "Cradle Games";
         }
 
- 
+        public class ModConfig
+        {
+            public bool ghost_nightmare = false;
+        }
+
+        private const string CONFIG_PATH = @"Mods\EnergyFailure\config.xml";
+        private readonly XmlSerializer xmlSerializer = new XmlSerializer(typeof(ModConfig));
+
+        public ModConfig config;
+
         public static bool InMenu => SceneManager.GetActiveScene().name.ToLower().Contains("empty");
         private static bool m_noCharacter = true;
         private float lightTimer = 0f;
         private float TurnOffTimer = 120f;
         private int flickerCount = 0;
-        private bool flickered = false;
+        private int maxFlickers = 3;
 
         public VoteCommand VoteManager => m_voteManager ?? (m_voteManager = TwitchManager.Instance?.GetComponentInChildren<VoteCommand>());
         private VoteCommand m_voteManager;
@@ -54,55 +62,61 @@ public class EnergyFailureClass : MelonMod
 
             lightTimer += UnityEngine.Time.deltaTime;
 
-            if (lightTimer > TurnOffTimer)
+            if (lightTimer >= TurnOffTimer)
             {
                 System.Random random = new System.Random();
                 int chance = random.Next(1, 101);
                 if (chance >= 95)
                 {
                     TurnOffLight();
+                    flickerCount = 0;
                     lightTimer -= TurnOffTimer;
-                    TurnOffTimer = random.Next(300, 361); ;
-                    //MelonLogger.Log("turning lights off");
+                    maxFlickers = random.Next(1, 5);
+                    TurnOffTimer = random.Next(300, 361);
+                    //MelonLogger.Log("next failure in " + TurnOffTimer + " seconds");
+                    //MelonLogger.Log("new flicker count " + maxFlickers);
                 }
                 else
                 {
                     LightFlicker();
+                    flickerCount++;
                     lightTimer -= TurnOffTimer;
                     TurnOffTimer = 2;
-                    flickerCount++;
                     //MelonLogger.Log("flickering");
-                    if (flickerCount >= 3)
+                    if (flickerCount >= maxFlickers)
                     {
                         flickerCount = 0;
-                        TurnOffTimer = random.Next(90,151);
-                        //MelonLogger.Log("reseting count and timer after flickering");
+                        lightTimer -= TurnOffTimer;
+                        maxFlickers = random.Next(1, 5);
+                        TurnOffTimer = random.Next(120,151);
+                        //MelonLogger.Log("next failure in " + TurnOffTimer + " seconds");
+                        //MelonLogger.Log("new flicker count " + maxFlickers);
                     }
                 }
-
             }
         }
 
         private void TurnOffLight()
         {
-            if (flickered)
-            {
-                LightSwitch.duration = 2;
+            System.Random random = new System.Random();
+                LightSwitch.duration = random.Next(1, 4);
                 LightSwitch.Apply();
-            }
-            else
-            {
-                LightSwitch.Apply();
-                LightSwitch.duration = 2;
-            }
+                int lightSwitchData = LightSwitch.duration;
+                //MelonLogger.Log("full shutdown, duration " + lightSwitchData + " minutes");
+
+                if (config.ghost_nightmare && lightSwitchData == 3)
+                {
+                    UDebug.Execute("spawn");
+                    //MelonLogger.Log("spawning ghosts");
+                }
         }
 
         private void LightFlicker()
         {
-            if (!flickered) flickered = true;
             LightSwitch.Apply();
             LightSwitch.duration = 0;
         }
+
         // Patches to track gameplay state
 
         [HarmonyPatch(typeof(SplashView), nameof(SplashView.OnSaveSelected))]
@@ -143,6 +157,49 @@ public class EnergyFailureClass : MelonMod
             {
                 m_noCharacter = true;
             }
+        }
+
+        // ======== Settings ========
+
+        public override void OnApplicationStart()
+        {
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            var folder = Path.GetDirectoryName(CONFIG_PATH);
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            else if (File.Exists(CONFIG_PATH))
+            {
+                var file = File.OpenRead(CONFIG_PATH);
+                var obj = xmlSerializer.Deserialize(file);
+                file.Close();
+
+                if (obj is ModConfig loadedConfig)
+                {
+                    config = loadedConfig;
+                    return;
+                }
+            }
+
+            // Only reach this point if valid settings not loaded
+            config = new ModConfig
+            {
+                ghost_nightmare = false,
+            };
+
+            SaveSettings();
+        }
+
+        private void SaveSettings()
+        {
+            FileStream file = File.Create(CONFIG_PATH);
+            xmlSerializer.Serialize(file, config);
+            file.Close();
         }
 
     }
